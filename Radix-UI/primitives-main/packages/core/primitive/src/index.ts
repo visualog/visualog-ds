b836787636c25b7860d55275d98c35a3276378ba
visualog <taikyong@gmail.com>
2025-08-07 17:01:41 +0900

    Initial commit: Add existing framework files

---
+++ Radix-UI/primitives-main/packages/core/primitive/src/index.ts
@@ -0,0 +1,2 @@
+export * from './primitive';
+export type * from './types';
---
+++ Radix-UI/themes-main/apps/playground/app/layout.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import './globals.css';
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+
+export const metadata = {
+  title: 'Radix Themes playground',
+  description: 'A playground of Radix Themes components',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Theme accentColor="brand">
+      {children}
+    </Theme>
+  );
+}
